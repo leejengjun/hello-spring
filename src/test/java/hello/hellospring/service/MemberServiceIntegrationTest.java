416295ac89eab72ef2936957f592b89297e15992
Jeongjun.lee <youngwoo4215@naver.com>
2022-08-18 17:10:00 +0900

    JPA 맛보기와 AOP 맛보기 코드예제

--- src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -24,7 +25,7 @@ class MemberServiceIntegrationTest {
     void 회원가입() {
         //given 이러한 상황이 주어져서
         Member member = new Member();
-        member.setName("spring");
+        member.setName("spring100");
 
         // when 뭔가 주어졌는데
         Long saveId = memberService.join(member);
